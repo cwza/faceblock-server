873e90d5b6eb8544c75b5768275ed85ea7729ce4
cwza <cwz0205a@gmail.com>
2016-11-25 20:16:35 +0800

    move initData initTable logic to function

--- routes/users.spec.js
+++ routes/users.spec.js
@@ -3,23 +3,29 @@ let expect = require('chai').expect;
 let db = require('../db').db;
 
 describe('route.users', function() {
-  let initUsers = [{mail: 'Test user 1'}, {mail: 'Test user 2'}];
+  let initUsers = null;
 
-  beforeEach(function() {
+  function *initTable() {
+    yield db.database.dropAllTable();
+    yield db.database.createAllTable();
+  }
+  function *initData() {
     initUsers = [{mail: 'Test user 1'}, {mail: 'Test user 2'}];
-    return db.tx(function *(t) {
-      yield db.database.dropAllTable();
-      yield db.database.createAllTable();
-      yield* initUsers.map((user, i) => {
-        return db.users.add(user)
-        .then((returnedUser) => {
-          initUsers[i] = returnedUser;
-        });
+    yield* initUsers.map((user, i) => {
+      return db.users.add(user)
+      .then((returnedUser) => {
+        initUsers[i] = returnedUser;
       });
+    });
+  }
+  beforeEach(function() {
+    return db.tx(function *(t) {
+      yield* initTable();
+      yield* initData();
     });
   });
   describe('GET /users/id', function() {
-    let url = 'http://localhost:3000/users/';
+    let url = 'http://localhost:3001/users/';
     it('get Test user 1', function() {
       return fetch(url + initUsers[0].id)
       .then((res) => {
