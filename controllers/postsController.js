b7d739435a7e2c7d24a29a889f6a2747ae7837b7
cwza <cwz0205a@gmail.com>
2016-12-07 20:30:09 +0800

    if nextPage has no record nextPage should be the same page

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -16,23 +16,46 @@ let queryParamsToParams = (queryParams) => {
   return params;
 }
 
+//if nextPage has no record nextPage will be the same to req
+// else nextPage will be page + 1
 let findByParams = (req) => {
   let params = queryParamsToParams(req.query);
-  console.log('params.page: ', typeof(params.page));
-  console.log('req.originalUrl: ', req.originalUrl);
-  console.log('nextUrl: ', utils.genNextPageUrl(req.originalUrl, params.page));
-  let nextUrl = domain + utils.genNextPageUrl(req.originalUrl, params.page !== undefined ? params.page : 1);
-  return db.posts.findByParams(params)
-    .then(data => {
+  params.page = params.page || 1;
+
+  let nextPagePromise = db.posts.findByParams(Object.assign({}, params, {page: params.page + 1}));
+  let thisPagePromise = db.posts.findByParams(params);
+  let promises = [nextPagePromise, thisPagePromise];
+  return Promise.all(promises)
+    .then( values => {
+      let nextPageData = values[0];
+      let thisPageData = values[1];
+      let nextUrl = domain;
+      if(nextPageData.length > 0)
+        nextUrl += utils.genNextPageUrl(req.originalUrl, params.page);
+      else
+        nextUrl += req.originalUrl;
       return {
         entities: {
-          posts: data.map(element => utils.deletePropertiesFromObject(element, ['score']))
+          posts: thisPageData.map(element => utils.deletePropertiesFromObject(element, ['score']))
         },
         links: {
           nextPage: nextUrl
         }
-      };
+      }
     });
+
+  // let nextUrl = domain + utils.genNextPageUrl(req.originalUrl, params.page);
+  // return db.posts.findByParams(params)
+  //   .then(data => {
+  //     return {
+  //       entities: {
+  //         posts: data.map(element => utils.deletePropertiesFromObject(element, ['score']))
+  //       },
+  //       links: {
+  //         nextPage: nextUrl
+  //       }
+  //     };
+  //   });
 }
 
 module.exports = {
