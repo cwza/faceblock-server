5a3f944fdbb481127b4b680972a2f27386da585f
cwza <cwz0205a@gmail.com>
2017-01-14 01:20:59 +0800

    fix typo

--- configs/configs.docker.js
+++ configs/configs.docker.js
@@ -6,7 +6,7 @@ let configs = {
     corsOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   },
   db: {
-    host: 'http://docker_faceblock_1',
+    host: 'http://docker_zombodb_postgres_1',
     port: 5432,
     database: 'postgres',
     user: 'postgres',
